86ec6f8285ca3eb3b0d57a490a928a4e05fbc109
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 08:20:29 -0300

    feat: orders migrations

---
+++ database/migrations/2025_05_22_121643_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("value", 8, 2);
+            $table->string("email");
+            $table->enum("status", ["pending", "paid", "cancelled", "refunded"]);
+            $table->string("address");
+            $table->string("city");
+            $table->string("state");
+            $table->string("postal_code");
+            $table->timestamp("paid_at")->nullable();
+            $table->foreignId("coupon_id")->nullable()->constrained("coupons");
+            $table->foreignId("product_id")->constrained("products");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
