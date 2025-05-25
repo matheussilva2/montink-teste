6c0a9b824545fab096da191efa1c60f4bfaacebe
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 10:27:28 -0300

    feat: api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CouponController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::resource("products", ProductController::class);
+Route::resource("coupons", CouponController::class);
+Route::resource("orders", OrderController::class);
