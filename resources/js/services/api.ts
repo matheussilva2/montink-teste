57959a7335dffb37d64e806bdb7d19fe047c700f
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 13:28:34 -0300

    feat: better config to api service

--- resources/js/services/api.ts
+++ resources/js/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8000/api/"
+    baseURL: "http://localhost:8000/api/",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
 });
