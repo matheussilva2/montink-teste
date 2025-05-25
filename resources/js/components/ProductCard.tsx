06af87b3ff97d340a5818738de344ea2acf6a266
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 10:26:26 -0300

    feat: product card

---
+++ resources/js/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+const ProductCard = ({product}) => {
+  return (
+    <div className="card" style={{width: 300, maxWidth: '100%'}}>
+      <div className="card-body">
+        <p className="h5 card-title text-center">{product.name}</p>
+      </div>
+      <div className="card-footer d-flex justify-content-between">
+        <button className="btn btn-success">Comprar</button>
+        <button className="btn btn-warning">Editar</button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
