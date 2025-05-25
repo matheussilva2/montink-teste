10114142e9e3ed9d23f6b1e930e071d0dc307690
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 13:32:50 -0300

    feat: remove useless import

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\CouponController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::resource("products", ProductController::class);
