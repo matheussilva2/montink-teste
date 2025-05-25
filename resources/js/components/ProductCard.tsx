166cbd6755c4303e7c50e07d6046d83dda888951
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 15:43:44 -0300

    feat: edit product button on product card

--- resources/js/components/ProductCard.tsx
+++ resources/js/components/ProductCard.tsx
@@ -1,4 +1,11 @@
-const ProductCard = ({product}) => {
+import { IProduct } from "@/types"
+
+type PropsType = {
+  product: IProduct,
+  selectProduct: (data: IProduct) => void
+}
+
+const ProductCard = ({product, selectProduct}: PropsType) => {
   return (
     <div className="card" style={{width: 300, maxWidth: '100%'}}>
       <div className="card-body">
@@ -6,7 +13,7 @@ const ProductCard = ({product}) => {
       </div>
       <div className="card-footer d-flex justify-content-between">
         <button className="btn btn-success">Comprar</button>
-        <button className="btn btn-warning">Editar</button>
+        <button className="btn btn-warning" onClick={() => selectProduct(product)}>Editar</button>
       </div>
     </div>
   )
