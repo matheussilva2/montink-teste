673db22411ed41fa9ab58b130cb79cd9681ddfab
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-25 15:56:30 -0300

    feat: show price on product card

--- resources/js/components/ProductCard.tsx
+++ resources/js/components/ProductCard.tsx
@@ -6,10 +6,12 @@ type PropsType = {
 }
 
 const ProductCard = ({product, selectProduct}: PropsType) => {
+  console.log(product);
   return (
     <div className="card" style={{width: 300, maxWidth: '100%'}}>
       <div className="card-body">
-        <p className="h5 card-title text-center">{product.name}</p>
+        <p className="h5 card-title">{product.name}</p>
+        <p>R$ {product.price}</p>
       </div>
       <div className="card-footer d-flex justify-content-between">
         <button className="btn btn-success">Comprar</button>
