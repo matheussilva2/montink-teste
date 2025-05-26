bb9b3c189f52c6ddd58fb9c4dda2c615d3e51919
Matheus <matheusnascimentoeoq@gmail.com>
2025-05-26 12:35:48 -0300

    feat: add webhook api route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+Route::post("orders/webhook", [OrderController::class, "webhook"]);
+
 Route::resource("products", ProductController::class);
 Route::resource("coupons", CouponController::class);
 Route::resource("orders", OrderController::class);
